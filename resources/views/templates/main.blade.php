3c39517dc5b83fd7f8ac88b5fa0234d4add06185
Shawn Welch <shrimpwagon@gmail.com>
2024-05-10 11:29:16 -0400

    Added reviews example page

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -24,6 +24,9 @@
                     <a class="nav-link" href="/features">Features</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="/reviews">Reviews</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="/services">Services</a>
                 </li>
                 <li class="nav-item">
